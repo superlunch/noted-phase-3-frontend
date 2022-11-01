82ae8e80d96424a4497628e5f21255916162cb8f
Scout Cox <coxscout@gmail.com>
2022-11-01 11:53:17 -0400

    Fought with search function until giving up :')

--- src/NoteCon.js
+++ src/NoteCon.js
@@ -1,16 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Notecards from "./Notecards";
 import Search from "./Search";
 
 function NoteCon({ notes, projects }) {
+  // const notesDisplay = notes.filter((notes) => (notes.project_id = 1));
+  const notesDisplayEl = notes.map((notes) => (
+    <Notecards key={notes.id} notes={notes} projects={projects} />
+  ));
+
+  // console.log(notesDisplay);
+  console.log(notesDisplayEl);
+
   return (
     <div className="home-container">
       <Search />
-      <div className="feed">
-        {notes.map((notes) => (
-          <Notecards key={notes.id} notes={notes} projects={projects} />
-        ))}
-      </div>
+      <div className="feed">{notesDisplayEl}</div>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,46 +1,50 @@
 import { React, useState } from "react";
 import Select from "react-select";
+import Notes from "./Notes";
+import Notecards from "./Notecards";
 
-const options = [
-  { value: null, label: "Filter by project..." },
-  { value: 1, label: "Home" },
-  { value: 2, label: "Work" },
-  { value: 3, label: "Etc." },
-  { value: 4, label: "Secret" },
-];
+function Search({ notes, projects }) {
+  // fetch("http://localhost:9292/projects")
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
 
-function Search() {
-  //   fetch("http://localhost:9292/projects")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     });
-  const [projectFilter, setProjectFilter] = useState(options);
+  const options = [
+    { value: 0, label: "Filter by project..." },
+    { value: 1, label: "Home" },
+    { value: 2, label: "Work" },
+    { value: 3, label: "Etc." },
+    { value: 4, label: "Secret" },
+  ];
 
-  function handleFilter() {
-    setProjectFilter(options.value);
-    console.log("filtering...");
+  function handleFilter(options) {
+    setFilterParam(options.value);
+    console.log(options.value);
   }
 
+  const [filterParam, setFilterParam] = useState(0);
+
   return (
-    <div className="home-container">
-      <div className="search-header" />
-      <div style={{ width: "300px" }}>
-        <Select
-          onChange={handleFilter}
-          options={options}
-          defaultValue={[options[0]]}
-          theme={(theme) => ({
-            ...theme,
-            borderRadius: 0,
-            width: 10,
-            colors: {
-              ...theme.colors,
-              primary25: "grey",
-              primary: "black",
-            },
-          })}
-        />
+    <div>
+      <div className="home-container">
+        <div className="search-header" />
+        <div style={{ width: "300px" }}>
+          <Select
+            onChange={handleFilter}
+            options={options}
+            defaultValue={[options[0]]}
+            theme={(theme) => ({
+              ...theme,
+              borderRadius: 0,
+              colors: {
+                ...theme.colors,
+                primary25: "grey",
+                primary: "black",
+              },
+            })}
+          />
+        </div>
       </div>
     </div>
   );
